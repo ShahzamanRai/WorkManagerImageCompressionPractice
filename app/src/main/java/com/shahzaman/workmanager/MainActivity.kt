d6122a3f95dd345ffa748753de48ddb436721613
shahzaman <shahzamanrai8@gmail.com>
2023-07-12 16:41:04 +0500

    Initial Commit

---
+++ app/src/main/java/com/shahzaman/workmanager/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.shahzaman.workmanager
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
+import coil.compose.AsyncImage
+import com.shahzaman.workmanager.ui.theme.WorkManagerTheme
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var workManager: WorkManager
+    private val viewModel by viewModels<PhotoViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        workManager = WorkManager.getInstance(applicationContext)
+        setContent {
+            WorkManagerTheme {
+                val workerResult = viewModel.workId?.let { id ->
+                    workManager.getWorkInfoByIdLiveData(id).observeAsState().value
+                }
+                LaunchedEffect(key1 = workerResult?.outputData) {
+                    if (workerResult?.outputData != null) {
+                        val filePath = workerResult.outputData.getString(
+                            PhotoCompressionWorker.KEY_RESULT_PATH
+                        )
+                        filePath?.let {
+                            val bitmap = BitmapFactory.decodeFile(it)
+                            viewModel.updateCompressedBitmap(bitmap)
+                        }
+                    }
+                }
+
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    viewModel.uncompressedUri?.let {
+                        Text(text = "Uncompressed Photo:")
+                        AsyncImage(model = it, contentDescription = null)
+                        Log.d(TAG, "onCreate: ${it.path}")
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    viewModel.compressedBitmap?.let {
+                        Text(text = "Compressed Photo:")
+                        AsyncImage(model = it, contentDescription = null)
+                        Log.d(TAG, "onCreate: ${it.generationId}")
+                    }
+                }
+            }
+        }
+
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        val uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent?.getParcelableExtra(Intent.EXTRA_STREAM, Uri::class.java)
+        } else {
+            intent?.getParcelableExtra(Intent.EXTRA_STREAM)
+        } ?: return
+        viewModel.updateUncompressUri(uri)
+
+        val request = OneTimeWorkRequestBuilder<PhotoCompressionWorker>().setInputData(
+                workDataOf(
+                    PhotoCompressionWorker.KEY_CONTENT_URI to uri.toString(),
+                    PhotoCompressionWorker.KEY_COMPRESSION_THRESHOLD to 1024 * 20L
+                )
+            ).build()
+        viewModel.updateWorkId(request.id)
+        workManager.enqueue(request)
+    }
+}
+
